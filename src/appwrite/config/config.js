4c446c5e03cb3c4535157baea08ef4e3b99bf84f
dushyantsolanki <dushyantsolanki.2002@gmail.com>
2023-12-11 00:31:01 +0530

    configuration file done

---
+++ src/appwrite/config/config.js
@@ -0,0 +1,7 @@
+const conf = {
+  projectId: String(import.meta.env.VITE_PROJECT_ID),
+  endPoint: String(import.meta.env.VITE_END_POINT),
+  apiKey: String(import.meta.env.VITE_API_KEY),
+};
+
+export default conf;
