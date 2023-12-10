86342855cd523e4f9289789cd2b89aebedba5285
dushyantsolanki <dushyantsolanki.2002@gmail.com>
2023-12-11 00:30:43 +0530

    authentication services done

---
+++ src/appwrite/services/authentication.js
@@ -0,0 +1,63 @@
+import { Client, Account, ID } from "appwrite";
+
+import conf from "../config/config";
+
+class Authentication {
+  client = new Client();
+  // appwrite = new Appwrite({
+  //   endpoint: conf.endPoint,
+  //   projectId: conf.projectId,
+  // });
+  account;
+  constructor() {
+    this.client.setEndpoint(conf.endPoint).setProject(conf.projectId);
+    this.account = new Account(this.client);
+  }
+
+  createAccount = async ({ email, password, name }) => {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      return userAccount;
+    } catch (error) {
+      console.log("appwrite : authentication :: createAccount :: ", error);
+      return false;
+    }
+  };
+
+  loginAccount = async ({ email, password }) => {
+    try {
+      const loginUser =  await this.account.createEmailSession(email, password);
+      return loginUser;
+    } catch (error) {
+      console.log("appwrite : authentication :: loginAccount :: ", error);
+      return false;
+    }
+  };
+
+  // loginWithGoogle = async () => {
+  //   try {
+  //     const userLoginGoogle = await this.appwrite.signInWithSocial("google");
+  //     return userLoginGoogle;
+  //   } catch (error) {
+  //     console.log("appwrite : authentication :: loginWithGoogle :: ", error);
+  //     return false;
+  //   }
+  // };
+
+  getLoginAccount =  async () => {
+    try {
+      const loginAccount = await this.account.get();
+      return loginAccount;
+    } catch (error) {
+      console.log("appwrite : authentication :: getLoginAccount :: ", error);
+      return false;
+    }
+}
+}
+const auth = new Authentication();
+export default auth;
