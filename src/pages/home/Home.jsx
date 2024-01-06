7d2563221559f80aef2b011227b933087c2bc181
dushyantsolanki <dushyantsolanki.2002@gmail.com>
2024-01-06 23:51:51 +0530

    remove : here i remove a unuses code

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,22 +1,9 @@
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-// import auth from "../../appwrite/services/authentication";
-import { useAuth } from "../../context/authcontext/authContext";
+import React from "react";
+
 import { Tldraw } from "@tldraw/tldraw";
 import "@tldraw/tldraw/tldraw.css";
 
 const Home = () => {
-  const { userData } = useAuth();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (!userData) {
-      // console.log(userData);
-      navigate("/login");
-    }
-    // console.log(userData);
-  }, []);
-
   return (
     <>
       <div style={{ position: "fixed", inset: 0 }}>
